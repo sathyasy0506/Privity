12f963ce5e09175c4d42569bee4383ecedb1a6b3
Sathya Sty <sathyasty@Sathyas-MacBook-Air.local>
2025-11-06 14:17:55 +0530

    form local url change

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "http://localhost/backend/";
-export const BASE_URL = "http://13.200.59.139/backend/";
+export const BASE_URL = "http://localhost/backend/";
+// export const BASE_URL = "http://13.200.59.139/backend/";
 
 export const ENDPOINTS = {
   CONTACT: () => `${BASE_URL}contact.php`,
